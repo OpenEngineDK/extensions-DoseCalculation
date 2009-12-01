2c46dd454d333b48e3eeae71230be41f000cff0d
ptx <ptx@openengine.dk>
2009-12-01 13:46:10 +0000

    Added cuda skeletton

---
+++ Renderers/CUDA/RayCaster.cu
@@ -0,0 +1,5 @@
+#include "RayCaster.h"
+
+void SetupRayCaster(const float* data, int w, int h, int d) {
+    
+}
---
+++ Renderers/CUDA/RayCaster.h
@@ -0,0 +1,8 @@
+#ifndef _DOSE_RAY_CASTER_H_
+#define _DOSE_RAY_CASTER_H_
+
+void SetupRayCaster(const float* data, int w, int h, int d);
+
+void RenderToPBO();
+
+#endif
