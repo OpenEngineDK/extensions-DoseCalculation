fa88e3425a6b7551e10270aec973184a17696f39
salomonhippie <salomonhippie@gmail.com>
2009-12-30 18:28:05 +0000

    added superposition kernels. disable compiler directive fpeel_loops if problems occur...

---
+++ Utils/CUDA/Superposition.h
@@ -0,0 +1,16 @@
+#ifndef _DOSE_SUPERPOSITION_H_
+#define _DOSE_SUPERPOSITION_H_
+
+#include <Scene/Beam.h>
+
+using OpenEngine::Scene::Beam;
+
+void Dose(float** out,
+          Beam oebeam,
+          Beam voi,
+          unsigned char* fmap, 
+          uint beamlets_x, uint beamlets_y,
+          int w, int h, int d,
+          float sw, float sh, float sd);
+
+#endif
